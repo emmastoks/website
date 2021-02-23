3b6a79f87a85496aba06a88bf20f3f24c14245c3
Emma Stoks <78866223+emmastoks@users.noreply.github.com>
2021-02-23 17:29:45 +0100

    Update Info.jsx

--- components/Coaches/Info.jsx
+++ components/Coaches/Info.jsx
@@ -15,50 +15,6 @@ const Info = () => {
               Mail&nbsp;us
             </a>
           </p>
-          {/*
-          <p>
-            *Applications are closed! Keep an eye on our{' '}
-            <ExternalLink href="https://twitter.com/osocode">Twitter account</ExternalLink> to see
-            when applications open for 2019 &mdash; or{' '}
-            <ExternalLink href="https://demodayosoc20.eventbrite.co.uk/" target="_blank">
-              join us on the Demo Day
-            </ExternalLink>{' '}
-            on the 30th of July.
-          </p>
-          */}
-        </div>
-        <div className="medium-8 large-6 cell large-offset-1">
-          <img
-            src="/img/pictures/osoc16-coaches-student-happy.jpg"
-            className="u-margin--b"
-            alt="A happy coach and a happy student"
-          />
-          <p>
-            <small>Open summer of code 2016 &mdash; a proud coach and a happy student</small>
-          </p>
-        </div>
-      </div>
-
-      <div className="grid-x grid-padding-x u-margin-large--tb">
-        <div className="medium-8 large-6 cell">
-          <img
-            src="/img/pictures/osoc16-harry-roberts.jpg"
-            className="u-margin--b"
-            alt="Students working out"
-          />
-          <p>
-            <small>
-              Harry Roberts{' '}
-              <ExternalLink
-                href="https://twitter.com/csswizardry"
-                title="Harry Roberts Twitter handle @csswizardry"
-              >
-                @csswizardry
-              </ExternalLink>{' '}
-              showing our students how CSS is done at open summer of code 2016.
-            </small>
-          </p>
-        </div>
         <div className="medium-8 large-5 cell large-offset-1">
           <p>
             Every student needs someone to teach them what cannot be taught in school. This is your
