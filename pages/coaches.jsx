4b8c45b212404db685576de5bec8596e42d4d8cd
Emma Stoks <78866223+emmastoks@users.noreply.github.com>
2021-02-26 10:20:25 +0100

    Update coaches.jsx

--- pages/coaches.jsx
+++ pages/coaches.jsx
@@ -1,20 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Coaches/Header';
 import Info from '../components/Coaches/Info';
-import Testimonials from '../components/Coaches/Testimonials';
 import FeaturedProjects from '../components/Projects/FeaturedProjects';
-
-const Coaches = () => (
-  <>
-    <Head>
-      <title>Coaches | Open Summer of Code</title>
-      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
-    </Head>
-    <Header />
-    <Info />
-    <Testimonials />
-    <FeaturedProjects divider="bg-lighter-gray" />
-  </>
-);
 
 export default Coaches;
